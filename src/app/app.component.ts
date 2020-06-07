a8a2e44eecc92597edea029448b099de56abc585
Eric BREHAULT <ebrehault@gmail.com>
2020-06-07 18:09:24 +0200

    enable Grange default views

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { GrangeViews } from '@guillotinaweb/grange';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'grange-demo';
+    constructor(
+        private views: GrangeViews,
+    ) {
+        this.views.initialize();
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { GrangeRootModule } from '@guillotinaweb/grange';
+import { TraversalModule } from 'angular-traversal';
+import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        GrangeRootModule.forRoot(),
+        StoreModule.forRoot({}),
+        TraversalModule,
+    ],
+    providers: [
+        {
+            provide: 'CONFIGURATION',
+            useValue: {
+                BACKEND_URL: 'http://127.0.0.1:8081/db/site',
+                CLIENT_TIMEOUT: 5000,
+            },
+        },
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
