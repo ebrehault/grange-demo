e744b727ebe32072c3e0f0a6445e44520c5433e9
Eric BREHAULT <ebrehault@gmail.com>
2020-06-20 12:06:39 +0200

    Display teams in home page

--- src/app/team-list/team-list.component.ts
+++ src/app/team-list/team-list.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { Grange } from '@guillotinaweb/grange';
+import { Observable } from 'rxjs';
+import { map, concatMap } from 'rxjs/operators';
+import { Resource } from '@guillotinaweb/grange-core';
 
 @Component({
     selector: 'app-team-list',
@@ -7,11 +10,26 @@ import { Grange } from '@guillotinaweb/grange';
 })
 export class TeamListComponent {
     newTeam = '';
+    teams: Resource[];
 
-    constructor(private grange: Grange) { }
-
-    addTeam() {
-        this.grange.addInContext({'@type': 'team', title: this.newTeam, contests: {}});
+    constructor(private grange: Grange) {
+        this.loadTeams().subscribe(teams => this.teams = teams);
     }
 
+    addTeam() {
+        this.grange.addInContext({'@type': 'team', title: this.newTeam, contests: {}}).onComplete.pipe(
+            concatMap(() => this.loadTeams()),
+        ).subscribe(teams => {
+            this.teams = teams;
+            this.newTeam = '';
+        });
+    }
+
+    loadTeams(): Observable<Resource[]> {
+        return this.grange.core.resource.items('/').pipe(
+            map(res => res.items
+                .filter(item => item['@type'] === 'team')
+            )
+        );
+    }
 }
